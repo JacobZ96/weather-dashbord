3b069284159daf225c7b6d9cbff4751750b8fa8e
WalleyemanZ <jacobzea96@gmail.com>
2023-02-05 16:23:17 -0700

    added fetch calls and elements i will use later

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,33 +11,73 @@
 var today = dayjs();
 var apiKey = "e474eaacc96b11909564331f7b9f7855"
 var searchBtnEl = document.querySelector('.btn')
+var curNameEl = document.getElementById('name');
+var curIconEl = document.getElementById('icon');
+var curTempEl = document.getElementById('Temp');
+var curHumEl = document.getElementById('Humidity');
+var curWindEl = document.getElementById('Wind');
+var searchCity = document.querySelector('input').value;
+
 document.getElementById('date').textContent = dayjs().format('(M/D/YYYY)');
 
 
-
+// don't forget to replace denver with ${city}
 var getWeather = function(city) {
-    var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
-    // var lat = "";
-    // var lon = "";
+    var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=denver&appid=${apiKey}`
+    var lonLatUrl= `https://api.openweathermap.org/data/2.5/forecast?lat="+ ${lat}&lon=${lon}&appid=${apiKey}`;
+    var lat = "";
+    var lon = "";
     fetch(geoUrl)
     .then(function(response) {
-        if (response.ok) {
             response.json()
             .then(function(data){
+                lat = data.lat;
+                lon = data.lon;
                 console.log(data);
+            
+            return fetch(lonLatUrl)
+            .then(function(response) {
+                return response.json()
+                .then(function(data){
+                console.log(data);
+            })
+            })
         })
-    }})
-}
-// var lonLatUrl= `https://api.openweathermap.org/data/2.5/forecast?lat="+ ${lat}&lon=${lon}&appid=${apiKey}`;
+    })
 
- var iconUrl = `http://openweathermap.org/img/wn/10d@2x.png`
+    
+    // fetch call to get the weather data 
+    // fetch(lonLatUrl)
+    // .then(function(response){
+    //     if (response.ok) {
+    //         response.json()
+    //         .then(function(data) {
+    //             console.log(data);
+    //         })
+    //     }
+    // })
+
+
+    // fetch call to get the weather icon
+    // var iconUrl = `http://openweathermap.org/img/wn/10d@2x.png`
+    // fetch(iconUrl)
+    // .then(function(response){
+    //     if (response.ok) {
+    //         response.json()
+    //         .then(function(data){
+    //             console.log(data);
+    //         })
+    //     }
+    // })
+
+}
+
 
 getWeather();
     
 
 
-searchBtnEl.addEventListener('click', getWeather());
-
+searchBtnEl.addEventListener('click', getWeather);
 
 // PSUDOCODING
 
